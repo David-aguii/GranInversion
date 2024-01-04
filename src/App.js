8b5f13dd047d47054aa6d94dbf3a2f5d4625a56a
David-aguii <davidaguii1927@gmail.com>
2024-01-04 12:15:33 -0300

    primer react

---
+++ src/App.js
@@ -0,0 +1,15 @@
+ 
+import './App.css';
+import ListaTareas from './ListaTareas';
+
+function App() {
+  return (
+    <div className="App">
+       <h1>Hello Dojo!</h1>
+       <h3>Things i need to do:</h3>
+       <ListaTareas/>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/ListaTareas.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const ListaTareas = () => {
+  return (
+        <ul>
+            <li>Learn react</li>
+            <li>Climb Mt. Everest</li>
+            <li>Run a marathon</li>
+            <li>Feed the dogs</li>
+        </ul>
+  );
+};
+
+export default ListaTareas;
