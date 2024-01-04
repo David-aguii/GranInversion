a1e54df5438a34c3c0536f4ec6004006716f94c0
David-aguii <davidaguii1927@gmail.com>
2024-01-04 13:21:23 -0300

    practico

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
  
-import './App.css';
-import ListaTareas from './ListaTareas';
+import PersonCard from './PersonCard';
+ 
 
 function App() {
   return (
-    <div className="App">
-       <h1>Hello Dojo!</h1>
-       <h3>Things i need to do:</h3>
-       <ListaTareas/>
+    <div className="App">   
+       <PersonCard firstName="Doe,Jane" age="45" hairColor="Black"/>
+       <PersonCard firstName="Smith,John" age="88" hairColor="Brown"/>
+       <PersonCard firstName="Fillmore,Millard" age="50" hairColor="Brown"/>
+       <PersonCard firstName="Smith,Maria" age="62" hairColor="Browm"/>
     </div>
   );
 }
